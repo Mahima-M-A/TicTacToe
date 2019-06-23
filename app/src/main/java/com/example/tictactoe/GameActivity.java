4fc2701d251500502ca76b5772238655b0033854
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-06-23 17:48:34 +0530

    Update GameActivity.java

--- app/src/main/java/com/example/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/tictactoe/GameActivity.java
@@ -354,6 +354,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         b9.setText("");
         control(true);
         turn = 0;
+        flag = 0;
         for(int i=0;i<3;i++)
             for(int j=0;j<3;j++)
                 table[i][j]=-1;
